b6f0319e52c9c334a515a70ae7feb590ffe80496
David Kariuki <16348353+damurka@users.noreply.github.com>
2025-10-28 10:24:23 +0300

    Updated the derived coverage plot and function

--- R/calculate_derived_coverage.R
+++ R/calculate_derived_coverage.R
@@ -31,12 +31,16 @@ calculate_derived_coverage <- function(.data, indicator) {
   penta1_denom <- get_population_column(indicator, 'penta1')
   penta1_derived_denom <- paste0(penta1_denom, 'derived')
   cov_penta1 <- paste0('cov_', indicator, '_penta1')
+  cov_anc1 <- paste0('cov_', indicator, '_anc1')
+  cov_dhis2 <- paste0('cov_', indicator, '_dhis2')
+  cov_un <- paste0('cov_', indicator, '_un')
   cov_penta1_derived <- paste0(cov_penta1, 'derived')
 
   data <- .data %>%
-    # select(any_of(c(group_vars)))
-    select(any_of(c(group_vars, 'year', population, 'population_yoy_change', 'population_survey_change',
-                    'population_proportion', indicator, penta1_denom, penta1_derived_denom, cov_penta1, cov_penta1_derived)))
+    select(any_of(c(group_vars, 'year', population, 'population_yoy_change',
+                    'population_survey_change', 'population_proportion', indicator,
+                    penta1_denom, penta1_derived_denom,
+                    cov_penta1, cov_anc1, cov_dhis2, cov_un, cov_penta1_derived)))
 
   # Return final tibble tagged with admin level
   new_tibble(
